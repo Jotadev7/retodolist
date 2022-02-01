e2c56722636d8a6dece56958eba960aa5607b11b
Joao <jotadev7@gmail.com>
2022-02-01 09:53:16 -0300

    Projeto revisão - desafio (Fake back-end ok, método get ok)

--- src/app/tarefas/models/tarefa.ts
+++ src/app/tarefas/models/tarefa.ts
@@ -1,8 +1,17 @@
-export interface Tarefa {
+export class Tarefa {
     id: number;
     titulo: string;
     descricao: string;
     finalizado: boolean;
     categoria: string;
     dataConclusao: Date;
+
+    constructor(id: number, titulo: string, descricao: string, finalizado: boolean, categoria: string, dataConclusao: Date) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.finalizado = finalizado;
+        this.categoria = categoria
+        this.dataConclusao = dataConclusao;
+    }
 }
--- src/app/tarefas/tarefa/tarefa.component.ts
+++ src/app/tarefas/tarefa/tarefa.component.ts
@@ -1,11 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { NgxSpinnerService, NgxSpinnerModule } from 'ngx-spinner';
-import { ActivatedRoute } from '@angular/router';
-import { ToastrService } from 'ngx-toastr';
-import { Subject } from 'rxjs';
+
 import { Tarefa } from '../models/tarefa';
-import { takeUntil } from 'rxjs/operators';
 import { TarefaService } from '../services/tarefa.service';
 
 @Component({
@@ -13,100 +8,27 @@ import { TarefaService } from '../services/tarefa.service';
   templateUrl: './tarefa.component.html'
 })
 
-export class TarefaComponent implements OnInit, OnDestroy {
+export class TarefaComponent implements OnInit {
 
-  public tarefaForm: FormGroup | any;
-  public titulo = 'Tarefas';
-  public tarefaSelecionada: Tarefa | any;
-  public textSimple: string = '';
-  Disable : boolean = true;
-  public orderby : string = "Categoria";
-  public categoria: object = {
-    Prioritaria: 4,
-    Urgente: 3,
-    Dificil: 2,
-    Facil: 1
-  }
-
-  private unsubscriber = new Subject();
-
+  constructor(private _tarefaService: TarefaService) {}
   public tarefas: Tarefa[] = [];
-  public tarefa: Tarefa | any;
-  public txtDeleteTarefa: string = '';
-  public tarefaSave = 'post';
-  public isAsc = true;
-  private _ORDERBY: object = {
-    dataConclusao: () => {
-      this.tarefas = this.isAsc ? this.tarefas.sort((a, b) => new Date(a.dataConclusao).getTime() - new Date(b.dataConclusao).getTime()) : this.tarefas.sort((a, b) => new Date(b.dataConclusao).getTime() - new Date(a.dataConclusao).getTime());
-    }
-  }
 
-  constructor(private _tarefaService: TarefaService,
-              private route: ActivatedRoute,
-              private fb: FormBuilder,
-              private toastr: ToastrService,
-              private spinner: NgxSpinnerService) { 
-      this.criarForm();
-  }
-
-  ngOnInit(): void {
-    this.carregarTarefas();
-  }
-
-  ngOnDestroy(): void {
-      this.unsubscriber.next();
-      this.unsubscriber.complete();
-  }
-
-  criarForm() {
-    this.tarefaForm = this.fb.group({
-      id: [0],
-      titulo: ['', Validators.required],
-      descricao: ['', Validators.required],
-      finalizado: [Boolean, Validators.required],
-      categoria: ['', Validators.required],
-      dataConclusao: [Date, Validators.required]
-    })
-  }
-
-  salvarTarefa() {
-    if(this.tarefaForm.valid) {
-      if(this.tarefaSave === 'post') {
-        this.tarefa = { ...this.tarefaForm.value};
-      } else {
-        this.tarefa = { id: this.tarefaSelecionada.id, ...this.tarefaForm.value};
-      }
-    }
-  }
-
-  deletarTarefa(id: number){
-    this._tarefaService.deletarTarefa(id).subscribe(
-      (tarefa: any) => {
-        console.log(tarefa);
-        this.toastr.success('Tarefa excluída com sucesso!');
-        this.carregarTarefas;
-      }
-    )
-  }
-
-  carregarTarefas() {
+  ngOnInit() {
     this._tarefaService.getTarefas()
       .subscribe(
-        tarefas => {
-        this.tarefas = tarefas;
-      },
-      error => console.log(error)
-    );
+        retorno => {
+          this.tarefas = retorno.map(item => 
+            {
+            return new Tarefa(
+              item.id,
+              item.titulo,
+              item.descricao,
+              item.finalizado,
+              item.categoria,
+              item.dataConclusao
+            )
+          })
+        }
+      )
   }
-
-  tarefaSelecionar(tarefa: Tarefa) {
-    this.tarefaSave = 'put';
-    this.tarefaSelecionada = tarefa;
-    this.tarefaForm.patchValue(tarefa);
-  }
-
-  limpar() {
-    this.tarefaSelecionada = null;
-  }
-
 }
