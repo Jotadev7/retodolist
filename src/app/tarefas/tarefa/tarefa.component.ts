bd483100b752dde21a12e8a9915ba31402a76a8e
Joao <jotadev7@gmail.com>
2022-02-07 17:01:22 -0300

    Modificações nos métodos do componente e estilização

--- src/app/tarefas/tarefa/tarefa.component.ts
+++ src/app/tarefas/tarefa/tarefa.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 
 import { Tarefa } from '../models/tarefa';
 import { TarefaService } from '../services/tarefa.service';
-import { takeUntil } from 'rxjs/operators';
-import { Observable, Subject } from 'rxjs';
+import { Observable } from 'rxjs';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
@@ -13,12 +12,10 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./tarefa.component.css']
 })
 
-export class TarefaComponent implements OnInit, OnDestroy {
-
-  private unsubscriber = new Subject();
+export class TarefaComponent implements OnInit {
 
   tarefaForm: FormGroup | any;
-  tarefa: Tarefa | any;
+  tarefa = {} as Tarefa;
   formResult: string = '';
   public tarefas: Tarefa[] = [];
 
@@ -26,12 +23,13 @@ export class TarefaComponent implements OnInit, OnDestroy {
               private fb: FormBuilder,
               private toastr: ToastrService) {}
               
-
+  // Ao iniciar, cria o formulário e carrega todas as tarefas.
   ngOnInit() {
     this.criarForm();
     this.carregarTarefas();
   }
 
+  // Cria o formulário usando FormBuilder e validações básicas.
   criarForm() {
     this.tarefaForm = this.fb.group({
       id: [0],
@@ -43,53 +41,54 @@ export class TarefaComponent implements OnInit, OnDestroy {
     })
   }
 
-  ngOnDestroy(): void {
-      this.unsubscriber.next();
-      this.unsubscriber.complete();
+  // Define se a tarefa será criada ou atualizada, com base no ID ser diferente de indefinido. (Chama os serviços de adicionar ou alterar)
+  guardaTarefa() {
+    if(this.tarefa.id !== undefined) {
+      this._tarefaService.alterarTarefa(this.tarefa).subscribe(() => {
+        this.criarForm();
+        this.carregarTarefas();
+        this.toastr.success('Tarefa salva com sucesso!', 'Sucesso!', {positionClass: 'toast-top-left', timeOut: 5000});
+      })
+    } else {
+      this._tarefaService.adicionarTarefa(this.tarefa).subscribe(() => {
+        this.criarForm();
+      })
+    }
   }
 
+  // Antigo método para salvar
+  // guardaTarefa(){
+  //   this.tarefa = Object.assign({}, this.tarefa, this.tarefaForm.value);
+  //   this._tarefaService.adicionarTarefa(this.tarefa).subscribe(
+  //     () => {
+  //       this.carregarTarefas();
+  //       this.toastr.success('Tarefa salva com sucesso!', 'Sucesso!', {positionClass: 'toast-top-left', timeOut: 5000});
+  //       this.criarForm();
+  //     }
+  //   );
+  // }
+
+  // Chama o serviço para carregar todas as tarefas
   carregarTarefas() {
     this._tarefaService.getTarefas()
-      .pipe(takeUntil(this.unsubscriber))
       .subscribe((tarefas: Tarefa[]) => {
         this.tarefas = tarefas;
     })
   }
 
-  guardaTarefa(){
-    this.tarefa = Object.assign({}, this.tarefa, this.tarefaForm.value);
-    this._tarefaService.adicionarTarefa(this.tarefa).subscribe(
-      () => {
-        this.carregarTarefas();
-        this.toastr.success('Tarefa salva com sucesso!', 'Sucesso!', {positionClass: 'toast-top-left', timeOut: 5000});
-        this.criarForm();
-      }
-    );
-  }
-
-  update(taref : FormData){
-    
-  }
-
-  atualizarTarefa() {
-    const { data } = this.tarefaForm.getRawValue()
-    this.tarefa = Object.assign({}, this.tarefa, this.tarefaForm.value);
-    console.log();
-    this._tarefaService.alterarTarefa(this.tarefa).subscribe(
-      sucesso => {
-        this.carregarTarefas();
-        this.toastr.success('Tarefa atualizada!');
-      }
-    )
-  }
-
+  // Remove uma tarefa (Chama o serviço de deletarTarefa passando uma tarefa)
   removeTarefa(tarefa: Tarefa) {
     this._tarefaService.deletarTarefa(tarefa).subscribe(
-      (tarefa: any) => {
-        console.log(tarefa);
+      () => {
         this.toastr.success('Tarefa excluída com sucesso!', 'Sucesso!', {positionClass: 'toast-top-center', timeOut: 5000});
         this.carregarTarefas();
       }
     )
   }
+
+  // Copia a tarefa para ser editada
+  atualizarTarefa(tarefa: Tarefa) {
+    this.tarefa = { ...tarefa };
+  }
+
 }
